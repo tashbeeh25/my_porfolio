2d5f5f2f9a4a2efa88851ba882b6508e00e35b5b
tashbeeh25 <tashbeiph@gmail.com>
2020-08-07 17:00:46 +0100

    Initial setup files for webpack and react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+//import { InstructorsContainer, InspirersContainer, StudentsContainer, MemesContainer, ChatContainer } from './containers'; 
+//import { NavBar, Title, NotFound404 } from './components';
+//import './styles/App.css';
+
+class App extends React.Component {
+    render() {
+      return (
+          <main>
+              
+          </main>
+      )
+    }
+  }
+export default App
